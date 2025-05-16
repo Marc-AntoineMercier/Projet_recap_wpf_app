1d6a633cdde867525c1e4e41a7a73bd1e4b38d98
Marc-AntoineMercier <marc.mercier.mam@gmail.com>
2025-05-15 21:23:42 -0400

    rajouter MainViewModel
    Rajouter ce MainViewModel pour pouvoir faire un ViewModel dans la View MainWindow

---
+++ IdeaManager.UI/ViewModels/MainViewModel.cs
@@ -0,0 +1,42 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using IdeaManager.UI.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdeaManager.UI.ViewModels
+{
+    public partial class MainViewModel : ViewModelBase
+    {
+        public NavigationService Navigator { get; }
+
+        private readonly IdeaFormViewModel _formViewModel;
+        private readonly IdeaListViewModel _listViewModel;
+        private readonly DashboardViewModel _dashboardViewModel;
+
+        public MainViewModel(
+            NavigationService navigationService,
+            IdeaFormViewModel formViewModel,
+            IdeaListViewModel listViewModel,
+            DashboardViewModel dashboardViewModel)
+        {
+            Navigator = navigationService;
+            _formViewModel = formViewModel;
+            _listViewModel = listViewModel;
+            _dashboardViewModel = dashboardViewModel;
+
+            Navigator.NavigateTo(_dashboardViewModel);
+        }
+
+        [RelayCommand]
+        private void ShowForm() => Navigator.NavigateTo(_formViewModel);
+
+        [RelayCommand]
+        private void ShowList() => Navigator.NavigateTo(_listViewModel);
+
+        [RelayCommand]
+        private void ShowDashboard() => Navigator.NavigateTo(_dashboardViewModel);
+    }
+}
