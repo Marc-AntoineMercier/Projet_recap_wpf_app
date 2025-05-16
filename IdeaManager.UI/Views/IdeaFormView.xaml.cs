a3a9877db020a47a9a59946fd900fa9b6c3b85b5
Marc-AntoineMercier <marc.mercier.mam@gmail.com>
2025-05-15 21:41:59 -0400

    faire View IdeaForm
    faire les IdeaFormView

--- IdeaManager.UI/Views/IdeaFormView.xaml.cs
+++ IdeaManager.UI/Views/IdeaFormView.xaml.cs
@@ -1,28 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows.Controls;
 
-namespace IdeaManager.UI.Views
+namespace IdeaManager.UI.Views;
+
+/// <summary>
+///     Logique d'interaction pour IdeaFormView.xaml
+/// </summary>
+public partial class IdeaFormView : UserControl
 {
-    /// <summary>
-    /// Logique d'interaction pour IdeaFormView.xaml
-    /// </summary>
-    public partial class IdeaFormView : Page
+    public IdeaFormView()
     {
-        public IdeaFormView()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
     }
 }
